636b94cffc989c1e7dc1150ff80aa5d976d1d61a
Atikur Rahaman <atikurahad@gmail.com>
2022-04-17 01:16:48 +0600

    added some new featcher

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import './About.css'
 
 const About = () => {
   return (
-    <div>
-      <h1 className='text-[red] text-center text-3xl'> I am from about</h1>
+    <div className='about-container'>
+      <p className="dev-about text-center text-white">
+        I'm a ReactJs based web developer and front‑end developer focused on
+        crafting clean and user‑friendly experiences. <br></br>I have 1+ years
+        of experience with HTML ,CSS, Bootstrap, JavaScript . I created full
+        responsive website with HTML,Css, Bootstrap and TailwindCss.<br></br> I
+        know Modern react hooks, useEffect , useState , useRef , useCallback
+        etc. <br></br> I am passionate about building excellent software that
+        improves the lives of those around me
+      </p>
     </div>
   );
 };
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import About from "../About/About";
 import "./Home.css";
 
 const Home = () => {
@@ -14,16 +15,15 @@ const Home = () => {
         </div>
 
         <div className="text-center dev-info">
-          <p className="text-[gold]  text-4xl mt-5">I'M ATIKUR RAHAMAN AHAD.</p>
-          <br></br>
-          <p className="text-white text-4xl"> WEB DEVELOPER</p>
+          <p className="text-[gold] dev-name mt-5">I'M ATIKUR RAHAMAN AHAD.</p>
+          <p className="text-[black] font-mono  text-4xl"> WEB DEVELOPER</p>
           <p className="text-[gray] text-2xl">
             I'm a Tunisian based web designer front end developer focused on
             crafting clean user friendly experiences, I am passionate about
             building excellent software that improves the lives of those around
             me.
           </p>
-          <button className=" button text-white">More About Me</button>
+          <button className=" button text-white" onClick={<About></About>}>More About Me</button>
         </div>
       </div>
     </>
